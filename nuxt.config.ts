04eeebc2ed1ac6d2aeca840e279d134f8ee6a0b6
Caio Emidio <caio.emidio@gmail.com>
2025-03-08 01:54:01 +0000

    testing vercel preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'cloudflare'
+    preset: "vercel",
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
